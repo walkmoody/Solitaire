d43ecbfb39eb99d3914d0ee039bc3de2a9f30420
Walker Moody <moodywalkertx@gmail.com>
2023-09-04 14:25:18 -0500

    Chaning menu screen

--- menus.cpp
+++ menus.cpp
@@ -11,21 +11,37 @@ void Menus::menusInit(){
 }
 
 string Menus::splash(){ // Slooping 
+    Image image = LoadImage("resources/icon.png");
+    ImageResize(&image, 300, 300);
+    Texture2D texture = LoadTextureFromImage(image);
+    UnloadImage(image);
     while (splashCount < 90){
 
         BeginDrawing();
 
                 ClearBackground(BLACK);
-                
-                DrawText("New Game", 250, 300, 30, LIGHTGRAY);
+                DrawTexture(texture, GetScreenWidth()/3, GetScreenHeight()/3, WHITE);   
+                DrawText("Solitaire", 250, 300, 30, LIGHTGRAY);
 
         EndDrawing();
         splashCount++;
     }
+    UnloadTexture(texture);
     return "menu";
 }
 
 string Menus::mainMenu(){
+    Image card = LoadImage("resources/cards/ace_of_spades.png");
+                       
+    ImageResize(&card, 200, 300);
+    Texture2D texture = LoadTextureFromImage(card);
+    UnloadImage(card);
+    int cardX =-200;
+    int cardX2 = -200 - GetScreenWidth()/2;
+    int cardY = 150;
+    int cardY2 = 150;
+    int count = 0;
+    int count2 = 0;
     while(!WindowShouldClose()){
         if(IsKeyDown(KEY_SPACE))
             return "game";
@@ -34,11 +50,35 @@ string Menus::mainMenu(){
         BeginDrawing();
 
             ClearBackground(GREEN);
-            
+            DrawTexture(texture, cardX, cardY, WHITE);   
+            DrawTexture(texture, cardX2, cardY2 - 400, WHITE);   
             DrawText("SOLITAIRE", 250, 300, 80, LIGHTGRAY);
 
         EndDrawing();
+        if(cardX2 < GetScreenWidth()){
+            cardX2 = cardX2 + 2;
+            cardY2 = cardY2 + 1;
+        }else if(count2 < 30)
+            count2++;
+        else {
+            cardX2 = -200;
+            cardY2 = 150;
+            count2 = 0;
         }
+
+        if(cardX < GetScreenWidth()){
+            cardX = cardX + 2;
+            cardY = cardY + 1;
+        }else if(count < 30)
+            count++;
+        else {
+            cardX = -200;
+            cardY = 150;
+            count = 0;
+        }
+
+        }
+    UnloadTexture(texture);
     return "quit";
 }
 
